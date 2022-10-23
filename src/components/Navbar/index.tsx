fd60a0d46a22bef0e01950520b4b56165b10a90f
nellfs <pokycelit@gmail.com>
2022-10-22 22:21:18 -0300

    navbar update

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,17 +5,20 @@ import ChevronDownIcon from "../../assets/utils/chevrondown_icon.png";
 const NavBar = () => {
   return (
     <div className="navbar">
-      <div className="container">
-        <div className="navbar__top-left">
-          <div className="number-contact">0800 088 1111</div>
-          <div>aA</div>
-          <img src={ThemeIcon}></img>
+      <div className="navbar__top">
+        <div className="container__top">
+          <div className="navbar__top-left">
+            <div className="number-contact">0800 088 1111</div>
+            <div>aA</div>
+            <img src={ThemeIcon}></img>
+          </div>
+          <div className="navbar__top-right">
+            <div>Varjota-CE</div>
+            <img src={ChevronDownIcon}></img>
+          </div>
         </div>
-        <div className="navbar__top-right">
-          <div>Varjota-CE</div>
-          <img src={ChevronDownIcon}></img>
-        </div>
-      </div>{" "}
+      </div>
+      <div className="navbar__bottom">hello</div>
     </div>
   );
 };
