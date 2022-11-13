a8e0a663ec604143740e46faf68108c97cb98e9f
nellfs <pokycelit@gmail.com>
2022-11-12 21:00:10 -0300

    todo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -29,68 +29,44 @@ const Home = () => {
             price={"79,90"}
           />
           <PlanCard
-            speed={100}
+            speed={300}
             description={"fibra"}
             bonus={["tocalivros", "nordestefc", "bebanca"]}
-            price={"79,90"}
-            bonus_info={true}
+            price={"89,90"}
           />
           <PlanCard
-            speed={100}
-            description={"fibra"}
-            bonus={["tocalivros", "nordestefc"]}
-            price={"79,90"}
-            bonus_info={true}
-          />
-          <PlanCard
-            speed={100}
-            description={"fibra"}
-            bonus={["tocalivros", "nordestefc", "bebanca"]}
-            price={"79,90"}
-            bonus_info={true}
-          />
-          <PlanCard
-            speed={100}
-            description={"fibra"}
-            bonus={["tocalivros", "nordestefc", "bebanca"]}
-            price={"79,90"}
-          />
-          <PlanCard
-            speed={100}
-            description={"fibra"}
-            bonus={["tocalivros", "nordestefc", "bebanca"]}
-            price={"79,90"}
-            bonus_info={true}
-          />
-          <PlanCard
-            speed={100}
-            description={"fibra"}
-            bonus={["tocalivros", "nordestefc", "bebanca"]}
-            price={"79,90"}
-            bonus_info={true}
-          />
-          <PlanCard
-            speed={100}
+            speed={300}
             description={"fibra"}
             bonus={["tocalivros", "nordestefc", "bebanca", "deezer"]}
-            price={"79,90"}
+            price={"99,90"}
             bonus_info={true}
           />
           <PlanCard
-            speed={100}
+            speed={500}
             description={"fibra"}
-            bonus={["tocalivros"]}
-            price={"79,90"}
+            bonus={["tocalivros", "nordestefc", "bebanca", "watchbr"]}
+            price={"109,90"}
+          />
+          <PlanCard
+            speed={500}
+            description={"fibra"}
+            bonus={["tocalivros", "nordestefc", "bebanca", "watchbr", "deezer"]}
+            price={"119,90"}
             bonus_info={true}
           />
           <PlanCard
-            speed={100}
+            speed={600}
             description={"fibra"}
-            bonus={["tocalivros"]}
-            price={"79,90"}
+            bonus={["tocalivros", "nordestefc", "bebanca", "watchbr", "hbomax"]}
+            price={"129,90"}
           />
+          <PlanCard speed={5} description={"radio"} price={"59,90"} />
+          <PlanCard speed={7} description={"radio"} price={"72,90"} />
+          <PlanCard speed={10} description={"radio"} price={"84,90"} />
+          <PlanCard speed={14} description={"radio"} price={"99,90"} />
         </div>
       </div>
+      update
     </div>
   );
 };
