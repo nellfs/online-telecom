8b15cac937643d3ef618a7da0cab3461b8f15782
nellfs <pokycelit@gmail.com>
2022-11-23 19:38:53 -0300

    newsletter p

--- src/components/LargeCard/NewsletterCard/NewsletterCard.tsx
+++ src/components/LargeCard/NewsletterCard/NewsletterCard.tsx
@@ -6,11 +6,13 @@ const NewsletterBanner = () => {
     <div className="newsletter">
       <div className="container">
         <img src={NewsletterImage} />
-        <small>CLUBE ONLINE</small>
-        <p>
-          Assine nossa newsletter e{' '}
-          <b>fique por dentro de todas as nossas ofertas!</b>
-        </p>
+        <div className="text">
+          <small>CLUBE ONLINE</small>
+          <p>
+            Assine nossa newsletter e{' '}
+            <b>fique por dentro de todas as nossas ofertas!</b>
+          </p>
+        </div>
       </div>
     </div>
   );
