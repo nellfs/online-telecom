7b657bf1dd5709ae4443399a29d7428cba1fe75b
nellfs <pokycelit@gmail.com>
2022-11-23 19:13:44 -0300

    newsletter image

--- src/components/LargeCard/NewsletterCard/NewsletterCard.tsx
+++ src/components/LargeCard/NewsletterCard/NewsletterCard.tsx
@@ -1,9 +1,17 @@
 import './NewsletterCard.css';
+import NewsletterImage from '../../../assets/images/newsletter-image.png';
 
 const NewsletterBanner = () => {
   return (
     <div className="newsletter">
-      <div className="container">Newsletter banner</div>
+      <div className="container">
+        <img src={NewsletterImage} />
+        <small>CLUBE ONLINE</small>
+        <p>
+          Assine nossa newsletter e{' '}
+          <b>fique por dentro de todas as nossas ofertas!</b>
+        </p>
+      </div>
     </div>
   );
 };
