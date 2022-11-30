e5317f16f8bee875698d0b51c3cbc49c853669be
nellfs <pokycelit@gmail.com>
2022-11-29 21:30:14 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import Location from "./pages/Location";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Location from './pages/Location';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Location></Location>}></Route>
-        <Route path="/:city/para-voce" element={<Home></Home>}></Route>
+        <Route path="/online-telecom" element={<Location></Location>}></Route>
+        <Route
+          path="/online-telecom/:city/para-voce"
+          element={<Home></Home>}
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
